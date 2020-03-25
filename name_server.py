043b841c1f64fbec2103a9cd3a7ebb1caa32d4a0
nbelisle <nicolebelisle121@gmail.com>
2020-03-25 15:24:03 -0400

    Added my name to server successfully

--- name_server.py
+++ name_server.py
@@ -1,5 +1,17 @@
 import requests
 
+request_json = {
+    "name": "Nicole Belisle",
+   "net_id": "nb202",
+   "e-mail": "nicole.belisle@duke.edu"
+}
+p = requests.post("http://vcm-7631.vm.duke.edu:5001/student",json=request_json)
+print(p)
+print(p.status_code)
+print(p.text)
+if p.status_code == 200:
+    print(p.json)
+
 r = requests.get("http://vcm-7631.vm.duke.edu:5001/list")
 answer = r.json()
 for student in answer:
